9b31ec7048e7fae917e28c83b7d8a7362f6365ee
mohdnavedshaikh01 <navedshafiqshaikh@gmail.com>
2024-03-24 21:35:43 -0400

    project steps

--- webScraping/webScraping.py
+++ webScraping/webScraping.py
@@ -1,1 +1,16 @@
-print("hello")
+#function One
+#download the webpage
+#function must take parameter of link and it returns a file path to an already downloaded html
+
+#function Two
+#it takes parameter of an html file path and uses beautifulSoup to filter required content and creates 
+#a new file and saves the filter data into it and returns file path of new file
+
+#function Three
+#takes in a parameter of filtered html file path and loads the data into beautifulSoup and goes through 
+#tags and find them and download them and replace the image tag(href) with # and returns a list of 
+#image file names
+
+
+
+
