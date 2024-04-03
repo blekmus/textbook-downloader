55e97d7b1688ccddfcdc706b97e03642fecbf559
blekmus <dinil@protonmail.com>
2024-04-03 13:09:06 -0400

    refactor web scraping function one

--- webScraping/webScraping.py
+++ webScraping/webScraping.py
@@ -1,38 +1,36 @@
-#function One
-#download the webpage
-#function must take parameter of link and it returns a file path to an already downloaded html
+# function Two
+# it takes parameter of an html file path and uses beautifulSoup to filter required content and creates
+# a new file and saves the filter data into it and returns file path of new file
 
-#function Two
-#it takes parameter of an html file path and uses beautifulSoup to filter required content and creates 
-#a new file and saves the filter data into it and returns file path of new file
+# function Three
+# takes in a parameter of filtered html file path and loads the data into beautifulSoup and goes through
+# tags and find them and download them and replace the image tag(href) with # and returns a list of
+# image file names
 
-#function Three
-#takes in a parameter of filtered html file path and loads the data into beautifulSoup and goes through 
-#tags and find them and download them and replace the image tag(href) with # and returns a list of 
-#image file names
+# look into https://github.com/SergeyPirogov/webdriver_manager for managing driver
 
 import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import tempfile
 
-from selenium import webdriver
 
-def downloadWebpage(webpageUrl):
-    # Set the path to the ChromeDriver executable
-    driverPath = "C:\\Users\\naved\\Downloads\\chromedriver_win32\\chromedriver.exe"
+def downloadWebpage(workingDir, webpageUrl, driverPath):
+    """
+    Downloads the webpage and saves it as an HTML file
+    :param webpageUrl: URL of the webpage to be downloaded
+    :param driverPath: Path to the chrome web driver
+    :return: Absolute path of the saved HTML file
+    """
 
     # Default output file path
-    outputFile = 'downloadedHtml.html'
+    outputFile = os.path.join(workingDir, 'webpage.html')
 
-    # Initialize chrome web driver
-    driver = webdriver.Chrome()
-
-    # Get webpage url
+    # Initialize chrome web driver and get the webpage
+    driver = webdriver.Chrome(driverPath)
     driver.get(webpageUrl)
 
-    # Getting html content
+    # Get html content
     webpageContent = driver.page_source
 
     # Close the webdriver
@@ -42,9 +40,10 @@ def downloadWebpage(webpageUrl):
     with open(outputFile, 'w', encoding='utf-8') as f:
         f.write(webpageContent)
 
-    print(f"Webpage downloaded and saved as '{outputFile}'")
+    print(f"Webpage downloaded and saved to '{outputFile}'")
 
-    return os.path.abspath(outputFile)
+    return outputFile
+
 
 def filterContentAndImages(htmlFilePath):
     # Read the html file
@@ -76,14 +75,20 @@ def filterContentAndImages(htmlFilePath):
     return os.path.abspath(filteredOutputFile)
 
 
-requiredFilePath = downloadWebpage(r"https://chem.libretexts.org/Bookshelves/Introductory_Chemistry/Basics_of_General_Organic_and_Biological_Chemistry_(Ball_et_al.)/02%3A_Elements_Atoms_and_the_Periodic_Table/2.02%3A_Atomic_Theory")
-print("Path of saved HTML file", requiredFilePath)
-htmlFilePath = requiredFilePath
+driverPath = "C:\\Users\\naved\\Downloads\\chromedriver_win32\\chromedriver.exe"
+url = "https://chem.libretexts.org/Bookshelves/Introductory_Chemistry/Basics_of_General_Organic_and_Biological_Chemistry_(Ball_et_al.)/02%3A_Elements_Atoms_and_the_Periodic_Table/2.02%3A_Atomic_Theory"
+
+# create a temporary directory to store data
+workingDir = tempfile.mkdtemp()
+
+# download the webpage
+htmlFilePath = downloadWebpage(workingDir, url, os.path.abspath(driverPath))
+print("Path of saved HTML file", htmlFilePath)
+
+# delete the temporary directory for now
+# remove it if you're using it later
+os.rmdir(workingDir)
+
+# get the main content from the html file
 filteredFilePath = filterContentAndImages(htmlFilePath)
-print("Path of filtered html file",filteredFilePath)
-
-
-
-
-
-
+print("Path of filtered html file", filteredFilePath)
