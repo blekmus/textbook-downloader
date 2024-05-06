dbc644ac8249a3de32b0a82ed9c42b6562ea52b5
blekmus <dinil@protonmail.com>
2024-05-06 12:22:53 -0400

    add credits

--- src/app.py
+++ src/app.py
@@ -23,18 +23,27 @@ class App(customtkinter.CTk):
         # configure grid layout
         self.grid_columnconfigure(4, weight=0)
         self.grid_columnconfigure((1, 2, 3, 5), weight=1)
-        self.grid_rowconfigure(1, weight=2)
-        self.grid_rowconfigure((2, 4), weight=0)
-        self.grid_rowconfigure((3, 6, 7), weight=1)
+        # self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure((1, 2, 3, 5), weight=0)
+        self.grid_rowconfigure((6, 7), weight=1)
 
         # title label
         self.logoLabel = customtkinter.CTkLabel(
             self,
             text="Webpage Downloader",
-            font=customtkinter.CTkFont(size=20, weight="bold"),
+            font=customtkinter.CTkFont(size=22, weight="bold"),
         )
         self.logoLabel.grid(
             row=1, column=1, columnspan=5, padx=20, pady=(20, 10), sticky="new"
+        )
+
+        self.credit = customtkinter.CTkLabel(
+            self,
+            text="Made by Dinil & Mohd Naved",
+            font=customtkinter.CTkFont(size=14),
+        )
+        self.credit.grid(
+            row=2, column=1, columnspan=5, padx=0, pady=(0, 20), sticky="new"
         )
 
         # save path label
@@ -42,13 +51,13 @@ class App(customtkinter.CTk):
             self, text="Save downloaded document to:", anchor="w"
         )
         self.savePathLabel.grid(
-            row=2, column=1, padx=20, pady=(10, 0), columnspan=5, sticky="nw"
+            row=3, column=1, padx=20, pady=(10, 0), columnspan=5, sticky="nw"
         )
 
         # save path entry
         self.saveEntry = customtkinter.CTkEntry(self)
         self.saveEntry.grid(
-            row=3, column=1, padx=20, pady=(10, 0), sticky="nwe", columnspan=4
+            row=4, column=1, padx=20, pady=(10, 0), sticky="nwe", columnspan=4
         )
         self.saveEntry.insert(0, self.appState.savePath)
 
@@ -57,13 +66,13 @@ class App(customtkinter.CTk):
             self, text="Select Folder", command=self.openSavePathSelector
         )
         self.savePathButton.grid(
-            row=3, column=4, padx=20, pady=(10, 0), sticky="ne", columnspan=2
+            row=4, column=4, padx=20, pady=(10, 0), sticky="ne", columnspan=2
         )
 
         # download path label
         self.urlLabel = customtkinter.CTkLabel(self, text="Document URL:", anchor="w")
         self.urlLabel.grid(
-            row=4, column=1, padx=20, pady=(10, 0), columnspan=5, sticky="nw"
+            row=5, column=1, padx=20, pady=(10, 0), columnspan=5, sticky="nw"
         )
 
         # download path entry
@@ -71,7 +80,7 @@ class App(customtkinter.CTk):
             self, placeholder_text="Paste the URL here"
         )
         self.urlEntry.grid(
-            row=5, column=1, padx=20, pady=(10, 0), sticky="nwe", columnspan=6
+            row=6, column=1, padx=20, pady=(10, 0), sticky="nwe", columnspan=6
         )
         self.urlEntry.insert(0, self.appState.downloadURL)
 
@@ -80,7 +89,7 @@ class App(customtkinter.CTk):
             self, text="Download", command=self.handleDownload
         )
         self.downloadButton.grid(
-            row=6, column=1, padx=20, pady=(10, 0), sticky="nw", columnspan=4
+            row=7, column=1, padx=20, pady=(10, 0), sticky="nw", columnspan=4
         )
 
     def openSavePathSelector(self):
